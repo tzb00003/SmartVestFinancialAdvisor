e023be8f3b4985011c5e8c4e36b781ca6da727c0
HChristian12 <chpiewet@gmail.com>
2026-01-29 02:12:38 -0500

    basic set up of the webpage create login and sign up bottons and add a side menus

--- SmartVestFinancialAdvisor/Program.cs
+++ SmartVestFinancialAdvisor/Program.cs
@@ -1,3 +1,4 @@
+using MudBlazor.Services;
 using SmartVestFinancialAdvisor.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
